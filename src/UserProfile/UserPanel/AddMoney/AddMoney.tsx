bf89fcb2cddc4d33380f9ed9ac7f2c2d1ac7af1a
Igor Stalmach <92231661+igorstalmach@users.noreply.github.com>
2023-01-27 16:26:08 +0100

    Implemented adding money

--- src/UserProfile/UserPanel/AddMoney/AddMoney.tsx
+++ src/UserProfile/UserPanel/AddMoney/AddMoney.tsx
@@ -1,17 +1,29 @@
 import React, {ChangeEvent} from "react";
 import './AddMoney.sass';
+import axios from "axios";
 
-interface Props {
-    setBalance: React.Dispatch<React.SetStateAction<number | undefined>>
-}
 
-export function AddMoney(props: Props) {
+export function AddMoney() {
+    const handleAddMoneySubmit = (e: React.FormEvent<HTMLButtonElement>) => {
+        e.preventDefault()
+
+        const postData = {
+            "money": String(document.getElementsByTagName("input")[0].value),
+            "userName": "customer"
+        }
+
+        axios.patch("http://192.168.1.142:8080/api/user/balance", postData).then(response => {
+            window.location.reload();
+        })
+    }
+
     return(
         <div className={'add-money-container'}>
             <form>
                 <label>
                     <p className='add-money-text'>Wpisz kwotę:</p>
-                    <input type="number" onChange={(e) => props.setBalance(parseInt(e.currentTarget.value))} />
+                    <input type="number"/>
+                    <button className='rented-scooter-button' id='add-money-button' onClick={(e) => handleAddMoneySubmit(e)}>Doładuj</button>
                 </label>
             </form>
         </div>
