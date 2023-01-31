6879ca446dd18ce8e4cac66e628a2e1ad157e357
Igor Stalmach <92231661+igorstalmach@users.noreply.github.com>
2023-01-31 23:51:58 +0100

    protection from adding an empty string of money

--- src/User/UserProfile/UserPanel/AddMoney/AddMoney.tsx
+++ src/User/UserProfile/UserPanel/AddMoney/AddMoney.tsx
@@ -1,19 +1,40 @@
 import './AddMoney.sass';
 import axios from "axios";
+import Swal from 'sweetalert2';
 
 
 export function AddMoney() {
     function handleAddMoneySubmit(e: React.FormEvent<HTMLButtonElement>) {
         e.preventDefault()
 
+        const inputValue = document.getElementsByTagName("input")[0].value;
+
         const postData = {
-            "money": String(document.getElementsByTagName("input")[0].value),
+            "money": String(inputValue),
             "userName": "customer"
         }
 
-        axios.patch("http://192.168.1.142:8080/api/user/balance", postData).then(response => {
-            window.location.reload();
+        const Toast = Swal.mixin({
+          toast: true,
+          position: 'top-end',
+          showConfirmButton: false,
+          timer: 3000,
+          timerProgressBar: true,
+          didOpen: (toast) => {
+            toast.addEventListener('mouseenter', Swal.stopTimer)
+            toast.addEventListener('mouseleave', Swal.resumeTimer)
+          }
         })
+
+        if (inputValue === "") {
+            Toast.fire({
+              title: 'Wpisz odpowiednią wartość'
+            })
+        } else {
+            axios.patch("http://192.168.1.142:8080/api/user/balance", postData).then(response => {
+                window.location.reload();
+            })
+        }
     }
 
     return(
