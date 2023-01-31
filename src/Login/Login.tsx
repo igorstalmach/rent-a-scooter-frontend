dccc27ab12b0ff732365a77bae3d3639d5d72fd6
Igor Stalmach <92231661+igorstalmach@users.noreply.github.com>
2023-01-31 23:51:31 +0100

    local protection from incorrect login & password

--- src/Login/Login.tsx
+++ src/Login/Login.tsx
@@ -1,6 +1,7 @@
 import './Login.sass';
 import React from "react";
 import {useNavigate} from "react-router-dom";
+import Swal from 'sweetalert2';
 
 
 interface Props {
@@ -13,8 +14,30 @@ export function Login(props: Props) {
 
     function handleClick(e: React.FormEvent<HTMLButtonElement>) {
         e.preventDefault();
-        props.setLogin(document.getElementsByTagName("input")[0].value);
-        navigate('/rent');
+
+        const loginValue = document.getElementsByTagName("input")[0].value;
+        const passwordValue = document.getElementsByTagName("input")[1].value;
+
+        const Toast = Swal.mixin({
+          toast: true,
+          position: 'top-end',
+          showConfirmButton: false,
+          timer: 3000,
+          timerProgressBar: true,
+          didOpen: (toast) => {
+            toast.addEventListener('mouseenter', Swal.stopTimer)
+            toast.addEventListener('mouseleave', Swal.resumeTimer)
+          }
+        })
+
+        if (!(loginValue === "customer" || loginValue === "serviceman" || loginValue === "admin") || (!(passwordValue === "1234"))) {
+            Toast.fire({
+              title: 'Nieprawidłowe dane'
+            })
+        } else {
+            props.setLogin(loginValue);
+            navigate('/rent');
+        }
     }
 
     return(
